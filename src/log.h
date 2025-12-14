04179187a330811f2e6d170686fe76ae28e9499d
TSCBRAD <1802brad@gmail.com>
2025-12-14 22:10:12 +0000

    fixed default function wrapper

--- src/log.h
+++ src/log.h
@@ -10,5 +10,8 @@
 
 extern const std::function<void(std::string_view, const std::source_location&)> log_message;
 
+inline void log(std::string_view message, const std::source_location& location = std::source_location::current()) {
+    log_message(message, location);
+}
 
 #endif
