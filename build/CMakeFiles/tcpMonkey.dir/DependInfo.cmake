3382a707d06be1fe33f352d5a1124c8ace390e21
brandon <brantylrobinson@gmail.com>
2025-12-14 19:19:54 +0000

    add build

---
+++ build/CMakeFiles/tcpMonkey.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/brandon/Projects/broadcastServer/src/log.cpp" "CMakeFiles/tcpMonkey.dir/src/log.cpp.o" "gcc" "CMakeFiles/tcpMonkey.dir/src/log.cpp.o.d"
+  "/home/brandon/Projects/broadcastServer/src/main.cpp" "CMakeFiles/tcpMonkey.dir/src/main.cpp.o" "gcc" "CMakeFiles/tcpMonkey.dir/src/main.cpp.o.d"
+  "" "tcpMonkey" "gcc" "CMakeFiles/tcpMonkey.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
